c2d327077f61272e3689cc443c55418f88773937
valentinefleith <valentine.fleith@gmail.com>
2024-03-22 12:28:45 +0100

    ajout premiere classification

--- src/classification.py
+++ src/classification.py
@@ -1,44 +1,43 @@
-import pandas as pd
 import sys
-
-class FileLoader:
-    def load(self, path):
-        df = pd.read_csv(path)
-        rows, columns = df.shape
-        print(f"Loading dataset of dimensions {rows} x {columns}")
-        return df
-
-    def display(self, df, n=None):
-        if n is not None:
-            if n >= 0:
-                print(df.head(n))
-            else:
-                print(df.tail(-n))
-        else:
-            print(df)
+from pretaitement import get_data, FileLoader
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import cross_val_predict, cross_val_score, train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay, precision_score, recall_score, f1_score
+import matplotlib.pyplot as plt
+import numpy as np
 
 
-def get_data(df):
-    data = df[["Texte", "Pol positive", "Pol negative"]]
+def get_matrix(corpus):
+    vectorizer = TfidfVectorizer(input="content", stop_words="english")
+    matrix = vectorizer.fit_transform(corpus).toarray()
+    return matrix
 
-    pol_positive = data["Pol positive"] > data["Pol negative"]
-    pol_negative = data["Pol negative"] > data["Pol positive"]
-    phrases_pos = data[pol_positive]
-    phrases_neg = data[pol_negative]
-    corpus_pos = phrases_pos["Texte"].to_list()
-    corpus_neg = phrases_neg["Texte"].to_list()
-    corpus = corpus_pos + corpus_neg
-    classes = ["Positive"] * len(corpus_pos) + ["Negative"] * len(corpus_neg)
-    print(len(corpus), len(classes))
-
+def naive_bayes_classification(matrix, classes):
+    nb_classifier = MultinomialNB()
+    y_pred = cross_val_predict(nb_classifier, matrix, classes, cv=10)
+    accuracy = accuracy_score(classes, y_pred)
+    precision = precision_score(classes, y_pred, average="macro")
+    recall = recall_score(classes, y_pred, average="macro")
+    f_score = f1_score(classes, y_pred, average="macro")
+    conf_matrix = confusion_matrix(classes, y_pred)
+    disp = ConfusionMatrixDisplay(conf_matrix, display_labels=np.unique(classes))
+    disp.plot(cmap="OrRd")
+    plt.show()
+    return accuracy, precision, recall, f_score
 
 def main():
     if len(sys.argv) != 2:
         sys.exit("Il faut un csv en argument.")
     loader = FileLoader()
     file = loader.load(sys.argv[1])
-    get_data(file)
-
+    corpus, classes = get_data(file)
+    matrix = get_matrix(corpus)
+    accuracy, precision, recall, f_score = naive_bayes_classification(matrix, classes)
+    print(f"Accuracy: {accuracy}")
+    print(f"precision: {precision}")
+    print(f"recall: {recall}")
+    print(f"f_score: {f_score}")
 
 if __name__ == "__main__":
     main()
